9743af3369cae3e7d88f1d5c243a1bb0b8d114b4
anemiaxb <59872864+anemiaxb@users.noreply.github.com>
2020-03-18 16:10:31 -0400

    Create Income_Cal.py

---
+++ Income_Cal.py
@@ -0,0 +1,52 @@
+"""
+Date: 3/18/2020
+Version: 0.1
+Owner: anemiaxb
+Author:
+Rationale:
+"""
+
+"""
+
+Cal's intro.
+
+The user will answer questions pertaining to pay type. Including hourly rate, OT rate, and if they know their tax rate.
+
+"""
+
+welcome = "Welcome! My name is Cal and I will be helping you today. To get started, I need some information about your income."
+
+print(welcome)
+
+# While loop until user inputs "Hourly" or "Salary"
+
+pay_type = ['Hourly', 'Salary']
+
+user_schedule = None
+
+while user_schedule not in pay_type:
+    user_schedule = input('What is your pay type? Hourly or Salary: ').title().strip()
+
+# Asking user hourly rate or gross salary and pay frequency
+
+if user_schedule == 'Salary':
+
+    user_salary = float(input('Gross Annual Salary: $ ').strip().replace(',', ''))  # float
+
+    user_salary_pay_frequency = input('Are you paid (W)eekly, (B)i-weekly, or (M)onthly?: ').strip().title()
+
+else:
+
+    user_hourly_rate = float(input('What is your hourly rate?: $ ').strip())  # float
+
+# Asking user for tax rate, if not known, then will calculate using previous paycheck info
+
+user_tax = input('Do you know what your tax rate is? Y or N: ').strip().upper()
+
+if user_tax == 'Y':
+
+    user_tax_rate = float(input('Tax rate: ').strip())
+
+else:
+
+    print('For me to figure out your tax rate, I will need information from your LAST paycheck.')
